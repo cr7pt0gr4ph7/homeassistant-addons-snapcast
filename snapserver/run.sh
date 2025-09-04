a105dd2d31cb9a1bcd753b0c58f3bebaba3eb240
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-09-04 19:48:26 +0200

    Add Snapserver addon

---
+++ snapserver/run.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bashio
+
+mkdir -p /share/snapfifo
+mkdir -p /share/snapcast
+
+config=/etc/snapserver.conf
+
+if ! bashio::fs.file_exists "${config}"; then
+    touch "${config}" ||
+        bashio::exit.nok "Could not create ${config} file on filesystem"
+fi
+bashio::log.info "Populating ${config}..."
+
+# Start creation of configuration
+
+echo "[stream]" > "${config}"
+for stream in $(bashio::config 'stream.streams'); do
+    echo "stream = ${stream}" >> "${config}"
+done
+echo "buffer = $(bashio::config 'stream.buffer')" >> "${config}"
+echo "codec = $(bashio::config 'stream.codec')" >> "${config}"
+echo "send_to_muted = $(bashio::config 'stream.send_to_muted')" >> "${config}"
+echo "sampleformat = $(bashio::config 'stream.sampleformat')" >> "${config}"
+
+echo "[http]" >> "${config}"
+echo "enabled = $(bashio::config 'http.enabled')" >> "${config}"
+echo "doc_root = $(bashio::config 'http.docroot')" >> "${config}"
+
+echo "[tcp]" >> "${config}"
+echo "enabled = $(bashio::config 'tcp.enabled')" >> "${config}"
+
+echo "[logging]" >> "${config}"
+echo "debug = $(bashio::config 'logging.enabled')" >> "${config}"
+
+echo "[server]" >> "${config}"
+echo "threads = $(bashio::config 'server.threads')" >> "${config}"
+
+echo "[server]" >> "${config}"
+echo "datadir = $(bashio::config 'server.datadir')" >> "${config}"
+
+bashio::log.info "Starting Snapserver..."
+/usr/bin/snapserver -c "${config}"
